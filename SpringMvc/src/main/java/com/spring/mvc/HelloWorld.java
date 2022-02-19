c2e622202a382fa528e1abdb1d8ccfd4c0fcf94a
pchun <pchun@192.168.1.3>
2022-02-19 08:03:26 -0500

    Second commit from spring_mvc_2

--- SpringMvc/src/main/java/com/spring/mvc/HelloWorld.java
+++ SpringMvc/src/main/java/com/spring/mvc/HelloWorld.java
@@ -5,5 +5,6 @@ public class HelloWorld {
 	public static void main(String args[]) {
 		System.out.println("Hello World");
 		System.out.println("Commit from spring_mvc_2");
+		System.out.println("Second commit from spring_mvc_2");
 	}
 }
